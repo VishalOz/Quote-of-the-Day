7b3ac4b331b22741e40edd39094d8e9f91f53333
Vishal Sudasinghe <vishal.20240036@iit.ac.lk>
2025-07-08 01:27:48 +0530

    backend is done

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 import express from 'express'
-import "cors"
+import { cors } from 'cors'
 
-const PORT = 3000
+
+const PORT = 5000
 const app = express();
+app.use(cors())
 
+const quotes = [
+    "Life is what happens when you're busy making other plans. – John Lennon",
+  "The greatest glory in living lies not in never falling, but in rising every time we fall. – Nelson Mandela",
+  "Your time is limited, don't waste it living someone else's life. – Steve Jobs",
+  "If you look at what you have in life, you'll always have more. – Oprah Winfrey",
+  "The purpose of our lives is to be happy. – Dalai Lama"
+];
 
-app.get('/', (req, res) => {
-    res.send("Hto wada hto")
+app.get('/api/quote', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * quotes.length)
+    res.json({quote: quotes[randomIndex]});
 });
 
 app.listen(PORT, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
